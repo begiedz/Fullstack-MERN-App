3a0e891623883a384600dfbedb9c5cf4bf15ea76
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-14 11:06:00 +0200

    Add MongoDB connection via Mongoose

--- server/server.js
+++ server/server.js
@@ -1,22 +1,31 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
-//express app
+// express app
 const app = express();
 
-//middleware
+// middleware
 app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
-//routes
+// routes
 app.use('/api/workouts', workoutRoutes);
 
-//listening
-app.listen(process.env.PORT, () => {
-    console.log('listening on http://localhost:' + process.env.PORT);
-});
+// connect to db
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        // listening
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db, listening on http://localhost:' + process.env.PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
