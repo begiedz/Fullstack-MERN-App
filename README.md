bc7833eaab62eaf962960667f0535f2b58fd327c
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-14 10:23:14 +0200

    Add Express Router & API Routes

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Fullstack MERN Application
 
-## Excersise logger
+## Excercise logger
+
+API Endpoints
+// GET      /workouts       -> Gets all the worout documents
+// POST     /workouts       -> Creates a new workout document
+// GET      /workouts/:id   -> Gets a single workout document
+// DELETE   /workouts/:id   -> Deletes a single workout
+// PATCH    /workouts/:id   -> Updates a single workout
--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 //express app
 const app = express();
+
+//middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
 //routes
-app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to my app' });
-});
+app.use('/api/workouts', workoutRoutes);
 
 //listening
 app.listen(process.env.PORT, () => {
-    console.log('listening on http://localhost:4000');
+    console.log('listening on http://localhost:' + process.env.PORT);
 });
