f2abf8ec46cb52d655a1739a87d84319c0bd4786
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-16 10:25:21 +0200

    Update controller

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { getWorkouts, getWorkout, createWorkout } = require('../controllers/workoutController');
+const {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout,
+} = require('../controllers/workoutController');
 
 const router = express.Router();
 
@@ -13,13 +19,8 @@ router.get('/:id', getWorkout);
 router.post('/', createWorkout);
 
 // DELETE a new workout
-router.delete('/:id', (req, res) => {
-    res.json({ msg: 'DELETE a workout' });
-});
+router.delete('/:id', deleteWorkout);
 
 // UPDATE a new workout
-router.patch('/:id', (req, res) => {
-    res.json({ msg: 'UPDATE a workout' });
-});
-
+router.patch('/:id', updateWorkout);
 module.exports = router;
