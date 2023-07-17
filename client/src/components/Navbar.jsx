8c70b65299079290a732972d3dc7b9d81f16c083
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-17 17:53:42 +0200

    Create Basic React Skeleton

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="container">
+                <Link to="/">
+                    <h1>Workout Buddy</h1>
+                </Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
