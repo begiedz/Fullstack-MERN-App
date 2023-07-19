72460a9af7f00000f6e5af51a97def67fec13f43
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-19 18:23:41 +0200

    Add React Context

---
+++ client/src/context/WorkoutContext.jsx
@@ -0,0 +1,32 @@
+import { React, createContext, useReducer } from 'react';
+
+export const WorkoutsContext = createContext();
+
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS':
+            return {
+                workouts: action.payload
+            }
+        case 'CREATE_WORKOUT':
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }
+        default:
+            return state
+    }
+}
+
+const WorkoutContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(workoutsReducer, {
+        workouts: null
+    })
+
+    return (
+        <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </WorkoutsContext.Provider>
+    )
+}
+
+export default WorkoutContextProvider
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import { React, useEffect, useState } from 'react';
+import { React, useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 //components
 import WorkoutDetails from '../components/WorkoutDetails';
 import WorkoutsForm from '../components/WorkoutsForm';
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null);
+    const { workouts, dispatch } = useWorkoutsContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -13,7 +14,7 @@ const Home = () => {
             const json = await response.json();
 
             if (response.ok) {
-                setWorkouts(json);
+                dispatch({ type: 'SET_WORKOUTS', payload: json })
             }
         };
 
